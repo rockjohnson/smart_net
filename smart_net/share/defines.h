d480103b50a8718a7fa5a36d2c6d9880efeb031f
rock <rockjohnson.liu@gmail.com>
2011-02-11 17:03:51 +0800

    modify

--- smart_net/share/defines.h
+++ smart_net/share/defines.h
@@ -5,8 +5,16 @@
  *      Author: rock
  */
 
-#ifndef DEFINES_H_
-#define DEFINES_H_
+#ifndef __SMARTNET_DEFINES_H__
+#define __SMARTNET_DEFINES_H__
 
+#define __EPOLL__ (1)
+#define __SELECT__ (2)
+
+#if __PLATFORM__ == __PLATFORM_LINUX__
+#define __IO_EVT_NOTIFY_MECHANISM__ __EPOLL__
+#elif __PLATFORM__ == __PLATFORM_WINDOWS__
+#define __IO_EVT_NOTIFY_MECHANISM__ __SELECT__
+#endif
 
 #endif /* DEFINES_H_ */
