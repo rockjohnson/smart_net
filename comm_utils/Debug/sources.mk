b84dda47777854178d53d8eee8885f1cdc60694a
rock <rockjohnson.liu@gmail.com>
2011-10-10 13:21:26 +0800

    ok

--- comm_utils/Debug/sources.mk
+++ comm_utils/Debug/sources.mk
@@ -23,9 +23,9 @@ C_UPPER_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-utils \
-thread \
-memory \
-log \
-common \
+src/utils \
+src/thread \
+src/memory \
+src/log \
+src/common \
 
